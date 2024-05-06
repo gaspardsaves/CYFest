b91503031f37334eefb520241949d7746d8d1f09
gaspardsaves <166817933+gaspardsaves@users.noreply.github.com>
2024-05-06 11:10:34 +0200

    Premier commit

---
+++ interface.h
@@ -0,0 +1,54 @@
+#ifndef STRUCTURES
+#define STRUCTURES
+
+#include <stddef.h>
+
+//utilisateur
+typedef struct {
+    int admin; //1
+    int id;
+    char* password;
+    int** resa; // double tableau qui contient les concerts et les sièges associés à ces derniers;
+    int porte_feuille;
+}Utilisateur; 
+
+typedef struct {
+    int admin; //1
+    int id;
+    char* password;
+}Manager;
+
+// Concert                           --------------------------------
+typedef struct {
+    int etat_salle; 
+    int categorie;
+    int prix;
+    int nombre_personne;
+    int fosse; // fosse ou pas f?1:0
+    Utilisateur id[20];
+} Siege;
+
+typedef struct {
+    int** siege[10]; // double tableau qui va contenir  
+    int nb_range;
+    int nb_siege_range;
+    int nb_cat_par_ranger;
+//déterminer le la catégorie C
+// malloc les sièges
+} Salle; 
+
+
+typedef struct {
+    char guest;
+    int horaire_d;
+    int horaire_f;
+    Salle salle;
+} Concert;
+
+
+typedef struct {
+    int concert[10];
+// Tableau qui contient tout les concerts
+} Festival;
+
+#endif
---
+++ manager.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "interface.h"
+
+int main (){
+    Manager m;
+    m.id=69;
+    printf("%d\n", m.id);
+    return 0;
+}
