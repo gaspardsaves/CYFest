ab32e6b02047db5a881ea50a9dd28386b7626312
gaspardsaves <166817933+gaspardsaves@users.noreply.github.com>
2024-05-06 16:23:24 +0200

    Ajout makefile et modif tabulation

--- smartrobusnest.h
+++ smartrobusnest.h
@@ -1,5 +1,5 @@
 #ifndef VERIF_MALLOC
-#define VERIF_MALLOC
-#include <stddef.h>
-void verifmalloc(void* p);
+    #define VERIF_MALLOC
+    #include <stddef.h>
+    void verifmalloc(void* p);
 #endif
